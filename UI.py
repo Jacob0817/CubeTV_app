8223f861c0f3303aa1656528772b6a09af7089fd
Jacob Zhou <zzh839985914@gmail.com>
2019-01-02 21:00:32 +0800

    添加记分板功能
    记分板功能为正式版本功能之一

--- UI.py
+++ UI.py
@@ -6,9 +6,7 @@
 # @Version : $Id$
 
 from PyQt5 import QtCore, QtWidgets
-import sys
 import qtawesome
-from results import *
 
 
 class MainUi(QtWidgets.QMainWindow):
@@ -16,24 +14,36 @@ class MainUi(QtWidgets.QMainWindow):
         super().__init__()
         self.init_main()
         self.init_left_widget()
-        self.init_right_widget()
+        self.init_final_widget()
         self.init_assemble()
 
     def init_main(self):
-        self.setFixedSize(960, 700)
+        self.setFixedSize(960, 720)
         self.main_widget = QtWidgets.QWidget()  # 创建窗口主部件
         self.main_layout = QtWidgets.QGridLayout()  # 创建窗口主部件网格布局
         self.main_widget.setLayout(self.main_layout)  # 设置窗口主部件网格布局
-        self.close = QtWidgets.QPushButton(qtawesome.icon('fa.close', color='white'),'')
+        self.close = QtWidgets.QPushButton(qtawesome.icon('fa.close', color='white'), '')
         self.close.clicked.connect(QtCore.QCoreApplication.instance().quit)
-        self.close.setFixedSize(25, 25)
+        self.close.setFixedSize(30, 30)
         self.close.setStyleSheet('''
             QPushButton{
-                background: #F76677;
+                background: #FF6F61;
                 border-radius: 5px;
             }
             QPushButton:hover{
                 background: red;
+            }
+            ''')
+        self.reset = QtWidgets.QPushButton(qtawesome.icon('fa.refresh', color='white'), '')
+        self.reset.clicked.connect(lambda: self.doreset())
+        self.reset.setFixedSize(30, 30)
+        self.reset.setStyleSheet('''
+            QPushButton{
+                background: #FF6F61;
+                border-radius: 5px;
+            }
+            QPushButton:hover{
+                background: green;
             }
             ''')
 
@@ -43,18 +53,21 @@ class MainUi(QtWidgets.QMainWindow):
         self.left_widget.setObjectName('left_widget')
         self.left_layout = QtWidgets.QGridLayout()
         self.left_widget.setLayout(self.left_layout)
+        self.left_widget.setFixedWidth(150)
         # 创建左侧控件内部件
         # 标签
-        self.L_lable_1 = QtWidgets.QPushButton('标签1')
+        self.L_lable_1 = QtWidgets.QPushButton('决赛')
         self.L_lable_1.setObjectName('L_lable')
-        self.L_lable_2 = QtWidgets.QPushButton('标签2')
+        self.L_lable_2 = QtWidgets.QPushButton('选手资料')
         self.L_lable_2.setObjectName('L_lable')
         # 按钮
-        self.L_button_1 = QtWidgets.QPushButton('显示成绩')
+        # ,lable 1
+        self.L_button_1 = QtWidgets.QPushButton('1V1')
         self.L_button_1.setObjectName('L_button')
-        self.L_button_1.clicked.connect(lambda: self.search())
-        self.L_button_2 = QtWidgets.QPushButton('按钮2')
+        self.L_button_1.clicked.connect(lambda: self.final())
+        self.L_button_2 = QtWidgets.QPushButton('成绩栏')
         self.L_button_2.setObjectName('L_button')
+        # ,lable 2
         self.L_button_3 = QtWidgets.QPushButton('按钮3')
         self.L_button_3.setObjectName('L_button')
         self.L_button_4 = QtWidgets.QPushButton('按钮4')
@@ -63,6 +76,10 @@ class MainUi(QtWidgets.QMainWindow):
         self.L_button_5.setObjectName('L_button')
         self.L_button_5.clicked.connect(lambda: self.clear())
         self.L_x = QtWidgets.QPushButton('')
+        # 输入框
+        self.inputbox = QtWidgets.QLineEdit()
+        self.inputbox.setPlaceholderText('无符号成绩')
+        self.inputbox.hide()
         # 放置左侧控件内部件
         self.left_layout.addWidget(self.close, 0, 0, 1, 1)
         self.left_layout.addWidget(self.L_lable_1, 1, 0, 1, 3)
@@ -71,91 +88,227 @@ class MainUi(QtWidgets.QMainWindow):
         self.left_layout.addWidget(self.L_lable_2, 4, 0, 1, 3)
         self.left_layout.addWidget(self.L_button_3, 5, 0, 1, 3)
         self.left_layout.addWidget(self.L_button_4, 6, 0, 1, 3)
-        self.left_layout.addWidget(self.L_button_5, 7, 0, 1, 3)
-        self.left_layout.addWidget(self.L_x, 8, 0, 1, 3)
+        self.left_layout.addWidget(self.L_x, 7, 0, 1, 3)
+        self.left_layout.addWidget(self.inputbox, 8, 0, 1, 3)
+        self.left_layout.addWidget(self.L_button_5, 9, 0, 1, 3)
         # 左侧控件QSS
+        self.L_x.setFixedHeight(30)
+        self.inputbox.setFixedHeight(30)
         self.left_widget.setStyleSheet('''
             QWidget#left_widget{
-                background: #0E1A49;
-                border-top: 1px #0E1A49;
-                border-bottom: 1px #0E1A49;
-                border-left: 1px #0E1A49;
+                background: #FF6F61;
                 border-top-left-radius: 10px;
                 border-bottom-left-radius: 10px;
+
             }
             QPushButton{
                 color: white;
                 border: none;
+                font-size: 18px;
+                font-weight: 700;
+                font-family: "微软雅黑";
             }
             QPushButton#L_lable{
                 border: none;
                 border-bottom: 1px solid white;
-                font-size: 18px;
-                font-weight: 700;
-                font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+                font-size: 22px;
+                font-weight: 900;
+                font-family: "微软雅黑";
             }
             QPushButton#L_button:hover{
-                border-left: 4px solid red;
-                font-weight: 700;
+                border-left: 4px solid white;
+                font-weight: 900;
+                font-size: 20px;
             }
-            ''')
-
-    # 创建右侧控件,网格布局
-    def init_right_widget(self):
-        self.right_widget = QtWidgets.QWidget()
-        self.right_widget.setObjectName('right_widget')
-        self.right_layout = QtWidgets.QGridLayout()
-        self.right_widget.setLayout(self.right_layout)
-        # 创建右侧控件内部件
-        # 查询栏
-        self.search_widget = QtWidgets.QWidget()  # 搜索框
-        self.search_layout = QtWidgets.QGridLayout()  # 搜索框布局
-        self.search_widget.setLayout(self.search_layout)
-        self.search_icon = QtWidgets.QLabel(chr(0xf002) + ' ' + '搜索')
-        self.search_icon.setFont(qtawesome.font('fa', 16))
-        self.search_widget_text = QtWidgets.QLineEdit()
-        self.search_widget_text.setPlaceholderText('Place holder text')
-
-        self.result_widget = QtWidgets.QLabel()
-        self.result_layout = QtWidgets.QGridLayout()
-        self.result_widget.setLayout(self.result_layout)
-        self.result_widget.setText(' ')
-        # 放置右侧控件内部件
-        # 搜索栏内
-        self.search_layout.addWidget(self.search_icon, 0, 0, 1, 1)
-        self.search_layout.addWidget(self.search_widget_text, 0, 1, 1, 8)
-        # 放置搜索栏
-        self.right_layout.addWidget(self.search_widget, 0, 0, 1, 9)
-        self.right_layout.addWidget(self.result_widget, 1, 0, 1, 9)
-        # 右侧控件CSS
-        self.search_widget_text.setStyleSheet('''
             QLineEdit{
-                border: 1px solid gray;
-                width: 300px;
-                border-radius: 10px;
-                padding: 2px 4px;
+                border-radius: 5px;
             }
             ''')
-        self.right_widget.setStyleSheet('''
-            QWidget#right_widget{
+
+    # 创建右侧控件,决赛成绩栏,网格布局
+    def init_final_widget(self):
+        self.final_widget = QtWidgets.QWidget()
+        self.final_widget.setObjectName('final_widget')
+        self.final_layout = QtWidgets.QGridLayout()
+        self.final_widget.setLayout(self.final_layout)
+        self.final_widget.setFixedHeight(200)
+        self.final_widget.hide()
+        # 创建右侧控件内部件
+        # 第一行
+        self.p_1 = QtWidgets.QPushButton()
+        self.p_1.setObjectName('player_L')
+        self.p_1_layout = QtWidgets.QGridLayout()
+        self.p_1.setLayout(self.p_1_layout)
+        self.p_1.setText('-')
+        self.p_1.clicked.connect(lambda: self.setName(1))
+        # r1
+        self.p_1_1 = QtWidgets.QPushButton()
+        self.p_1_1.setObjectName('upper')
+        self.p_1_1_layout = QtWidgets.QGridLayout()
+        self.p_1_1.setLayout(self.p_1_1_layout)
+        self.p_1_1.setText('-')
+        self.p_1_1.clicked.connect(lambda: self.setResult(1))
+        # r2
+        self.p_1_2 = QtWidgets.QPushButton()
+        self.p_1_2.setObjectName('upper')
+        self.p_1_2_layout = QtWidgets.QGridLayout()
+        self.p_1_2.setLayout(self.p_1_2_layout)
+        self.p_1_2.setText('-')
+        self.p_1_2.clicked.connect(lambda: self.setResult(2))
+        # r3
+        self.p_1_3 = QtWidgets.QPushButton()
+        self.p_1_3.setObjectName('upper')
+        self.p_1_3_layout = QtWidgets.QGridLayout()
+        self.p_1_3.setLayout(self.p_1_3_layout)
+        self.p_1_3.setText('-')
+        self.p_1_3.clicked.connect(lambda: self.setResult(3))
+        # r4
+        self.p_1_4 = QtWidgets.QPushButton()
+        self.p_1_4.setObjectName('upper')
+        self.p_1_4_layout = QtWidgets.QGridLayout()
+        self.p_1_4.setLayout(self.p_1_4_layout)
+        self.p_1_4.setText('-')
+        self.p_1_4.clicked.connect(lambda: self.setResult(4))
+        # r5
+        self.p_1_5 = QtWidgets.QPushButton()
+        self.p_1_5.setObjectName('upper_last')
+        self.p_1_5_layout = QtWidgets.QGridLayout()
+        self.p_1_5.setLayout(self.p_1_5_layout)
+        self.p_1_5.setText('-')
+        self.p_1_5.clicked.connect(lambda: self.setResult(5))
+        # 第二行================================================
+        self.p_2 = QtWidgets.QPushButton()
+        self.p_2.setObjectName('player_R')
+        self.p_2_layout = QtWidgets.QGridLayout()
+        self.p_2.setLayout(self.p_2_layout)
+        self.p_2.setText('-')
+        self.p_2.clicked.connect(lambda: self.setName(2))
+        # r1
+        self.p_2_1 = QtWidgets.QPushButton()
+        self.p_2_1.setObjectName('lower')
+        self.p_2_1_layout = QtWidgets.QGridLayout()
+        self.p_2_1.setLayout(self.p_2_1_layout)
+        self.p_2_1.setText('-')
+        self.p_2_1.clicked.connect(lambda: self.setResult(6))
+        # r2
+        self.p_2_2 = QtWidgets.QPushButton()
+        self.p_2_2.setObjectName('lower')
+        self.p_2_2_layout = QtWidgets.QGridLayout()
+        self.p_2_2.setLayout(self.p_2_2_layout)
+        self.p_2_2.setText('-')
+        self.p_2_2.clicked.connect(lambda: self.setResult(7))
+        # r3
+        self.p_2_3 = QtWidgets.QPushButton()
+        self.p_2_3.setObjectName('lower')
+        self.p_2_3_layout = QtWidgets.QGridLayout()
+        self.p_2_3.setLayout(self.p_2_3_layout)
+        self.p_2_3.setText('-')
+        self.p_2_3.clicked.connect(lambda: self.setResult(8))
+        # r4
+        self.p_2_4 = QtWidgets.QPushButton()
+        self.p_2_4.setObjectName('lower')
+        self.p_2_4_layout = QtWidgets.QGridLayout()
+        self.p_2_4.setLayout(self.p_2_4_layout)
+        self.p_2_4.setText('-')
+        self.p_2_4.clicked.connect(lambda: self.setResult(9))
+        # r5
+        self.p_2_5 = QtWidgets.QPushButton()
+        self.p_2_5.setObjectName('lower_last')
+        self.p_2_5_layout = QtWidgets.QGridLayout()
+        self.p_2_5.setLayout(self.p_2_5_layout)
+        self.p_2_5.setText('-')
+        self.p_2_5.clicked.connect(lambda: self.setResult(10))
+        # 放置右侧控件内部件
+        # 放置结果输出，临时组件
+        self.final_layout.addWidget(self.p_1, 1, 0, 1, 1)
+        self.final_layout.addWidget(self.p_1_1, 1, 1, 1, 1)
+        self.final_layout.addWidget(self.p_1_2, 1, 2, 1, 1)
+        self.final_layout.addWidget(self.p_1_3, 1, 3, 1, 1)
+        self.final_layout.addWidget(self.p_1_4, 1, 4, 1, 1)
+        self.final_layout.addWidget(self.p_1_5, 1, 5, 1, 1)
+        self.final_layout.addWidget(self.p_2, 2, 0, 1, 1)
+        self.final_layout.addWidget(self.p_2_1, 2, 1, 1, 1)
+        self.final_layout.addWidget(self.p_2_2, 2, 2, 1, 1)
+        self.final_layout.addWidget(self.p_2_3, 2, 3, 1, 1)
+        self.final_layout.addWidget(self.p_2_4, 2, 4, 1, 1)
+        self.final_layout.addWidget(self.p_2_5, 2, 5, 1, 1)
+        self.final_layout.setSpacing(0)
+        # final控件QSS
+        self.final_widget.setStyleSheet('''
+            QWidget#final_widget{
                 color: white;
-                background: white;
-                border-top: 1px solid darkGray;
-                border-bottom: 1px solid darkGray;
-                border-right: 1px solid darkGray;
-                border-top-right-radius: 10px;
-                border-bottom-right-radius: 10px;
+                background: #0E1A49;
+                border-radius: 10px;
+            }
+            QPushButton{
+                color: white;
+                border: none;
+                font-size: 25px;
+                font-weight: 700;
+                font-family: "微软雅黑";
+                height: 100px
+            }
+            QPushButton#player_L{
+                font-size: 28px;
+                border: 2px solid darkGray;
+                border-left: none;
+                border-top: none;
+            }
+            QPushButton#upper{
+                border: 2px solid darkGray;
+                border-top: none;
+            }
+            QPushButton#upper:hover{
+                color: red;
+                font-size: 30px;
+                font-weight: 900;
+            }
+            QPushButton#upper_last{
+                border: 2px solid darkGray;
+                border-top: none;
+                border-right: none;
+            }
+            QPushButton#upper_last:hover{
+                color: red;
+                font-size: 30px;
+                font-weight: 900;
+            }
+            QPushButton#player_R{
+                font-size: 28px;
+                border: 2px solid darkGray;
+                border-left: none;
+                border-bottom: none;
+            }
+            QPushButton#lower{
+                border: 2px solid darkGray;
+                border-bottom: none;
+            }
+            QPushButton#lower:hover{
+                color: red;
+                font-size: 30px;
+                font-weight: 900;
+            }
+            QPushButton#lower_last{
+                border: 2px solid darkGray;
+                border-bottom: none;
+                border-right: none;
+            }
+            QPushButton#lower_last:hover{
+                color: red;
+                font-size: 30px;
+                font-weight: 900;
             }
             ''')
 
     def init_assemble(self):
         # 放置控件
         self.main_layout.addWidget(self.left_widget, 0, 0, 12, 2)
-        self.main_layout.addWidget(self.right_widget, 0, 2, 12, 10)
-        self.main_layout.addWidget(self.close, 0, 0, 1, 12)
+        self.main_layout.addWidget(self.final_widget, 0, 2, 12, 10)
+        self.main_layout.addWidget(self.close, 0, 0, 1, 1)
+        self.main_layout.addWidget(self.reset, 0, 1, 1, 12)
         # 设置主部件
         self.setCentralWidget(self.main_widget)
         self.setWindowOpacity(0.9)  # 设置窗口透明度
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)  # 设置透明窗口背景
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # 隐藏边框
-        self.main_layout.setSpacing(0)
+        self.main_layout.setSpacing(5)
--- main.py
+++ main.py
@@ -6,21 +6,82 @@
 # @Version : $Id$
 
 from UI import *
+from results import *
+import sys
 
 
 class function(MainUi):
     def __init__(self):
         super().__init__()
 
-    def search(self):
-        try:
-            results = n_results('2011ZHAN24', 444)[0]
-            self.result_widget.setText(str(results))
-        except:
-            self.result_widget.setText('未找到匹配项或未连接到数据库')
+    def final(self):
+        self.final_widget.show()
+        self.inputbox.show()
+        self.L_x.hide()
 
     def clear(self):
-        self.result_widget.setText(' ')
+        self.p_1.setText('-')
+        self.p_1_1.setText('-')
+        self.p_1_2.setText('-')
+        self.p_1_3.setText('-')
+        self.p_1_4.setText('-')
+        self.p_1_5.setText('-')
+        self.p_2.setText('-')
+        self.p_2_1.setText('-')
+        self.p_2_2.setText('-')
+        self.p_2_3.setText('-')
+        self.p_2_4.setText('-')
+        self.p_2_5.setText('-')
+
+    def doreset(self):
+        self.final_widget.hide()
+        self.inputbox.hide()
+        self.L_x.show()
+
+    def setName(self, id):
+        result = self.inputbox.text()
+        if (id == 1):
+            self.p_1.setText(result)
+        elif (id == 2):
+            self.p_2.setText(result)
+        self.inputbox.clear()
+
+    def _format(self, result):
+        time = int(result)
+        if (time >= 10000):
+            return (result[0] + ":" + result[1:3] + ":" + result[3:5])
+        elif (time < 10000 and time >= 1000):
+            return (result[0:2] + ":" +result[2:4])
+        elif (time < 1000 and time >= 100):
+            return (result[0] + ":" + result[1:3])
+
+    def setResult(self, id):
+        rresult = self.inputbox.text()
+        if rresult and (int(rresult) <= 99999) and (int(rresult) >99):
+            result = self._format(rresult)
+            if (id == 1):
+                self.p_1_1.setText(result)
+            elif (id == 2):
+                self.p_1_2.setText(result)
+            elif (id == 3):
+                self.p_1_3.setText(result)
+            elif (id == 4):
+                self.p_1_4.setText(result)
+            elif (id == 5):
+                self.p_1_5.setText(result)
+            elif (id == 6):
+                self.p_2_1.setText(result)
+            elif (id == 7):
+                self.p_2_2.setText(result)
+            elif (id == 8):
+                self.p_2_3.setText(result)
+            elif (id == 9):
+                self.p_2_4.setText(result)
+            elif (id == 10):
+                self.p_2_5.setText(result)
+        else:
+            pass
+        self.inputbox.clear()
 
 
 def main():
